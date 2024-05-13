ef23c842ff2ff0ddf6806715c1f4799e0aab54ff
TuyenH <thtuyen199@gmail.com>
2024-05-13 12:38:33 +0000

    Task2- display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+// Check if URL is provided
+if (process.argv.length < 3) {
+  console.log("Usage: node 2-statuscode.js <URL>");
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+// Make a GET request to the URL
+request.get(url, (error, response) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(`code: ${response.statusCode}`);
+});
