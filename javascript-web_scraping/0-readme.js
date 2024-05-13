d3e437bb8bf22b3deb268cb407c2337b47da303a
TuyenH <thtuyen199@gmail.com>
2024-05-13 12:16:19 +0000

    modify  align

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,17 +4,20 @@ const fs = require('fs');
 
 // Check if file path is provided
 if (process.argv.length < 3) {
-    console.log('Usage: node readme.js <file_path>');
-    process.exit(1);
+  console.log('Usage: node readme.js <file_path>');
+  process.exit(1);
 }
 
 const filePath = process.argv[2];
 
 // Read the content of the file
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err); // Print the error object if an error occurred
-    } else {
-        console.log(data); // Print the content of the file
-    }
+  // Print the error object if an error occurred
+  if (err) {
+    console.error(err);
+  }
+  // Print the content of the file
+  else {
+    console.log(data);
+  }
 });
