80f594aae9e40d59eadf4031b9a2943efd24247e
TuyenH <thtuyen199@gmail.com>
2024-05-14 08:56:09 +0000

    modify code to fetch content of a web page and store to a file without any comment

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -13,23 +13,13 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 // Make a GET request to fetch the content of the webpage
-request.get(url, (error, response, body) => {
-  if (error) {
-    console.error('Error', error);
-    return;
+request(url, function (err, reponse, body) {
+  if (err) {
+    console.log(err);
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data: Status ${response.statusCode}`);
-    return;
-  }
-
-  // Write the content to specified file path
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
+  fs.writeFile(filePath, body, 'utf8', function (err) {
     if (err) {
-      console.error('Error writing file:', err);
-      return;
+      console.log(err);
     }
-    console.log(`Webpage content has been saved to ${filePath}`);
   });
 });
