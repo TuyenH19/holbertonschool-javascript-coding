2b4cb59e729bb362c37bc4a8a6fd746bb5555444
TuyenH <thtuyen199@gmail.com>
2024-05-14 08:55:15 +0000

    modify code to write string to the file without any comment

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -12,11 +12,6 @@ const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
 // Write the string to the file
-fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err); // Print the error object if an error occurred
-  }
-  else {
-    console.log(`Successfully wrote "${stringToWrite}" to ${filePath}`);
-  }
+fs.writeFile(filePath, stringToWrite, 'utf8', function (err) {
+  if (err) console.log(err);
 });
