ca7ddf60b9c05f06523e025ec6de1856992c7138
TuyenH <thtuyen199@gmail.com>
2024-05-27 14:11:09 +0000

    Task4- Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port${port}`);
+});
+
+/* app.listen(port, (error) => {
+  if (error) {
+    console.log('Something went wrong', error);
+  } else {
+    console.log(`Server is listening on port${port}`);
+  }
+}); */
