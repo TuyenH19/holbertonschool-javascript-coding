32b8e7447d751aa116bb0c2c61901ab39ff92db4
TuyenH <thtuyen199@gmail.com>
2024-05-28 11:56:02 +0000

    rewrite the code

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,43 +1,22 @@
 const fs = require('fs');
 
-function countStudents(path) {
+const countStudents = (path) => {
   try {
-    // Read the file synchronously
-    const data = fs.readFileSync(path, 'utf8');
-
-    // Split the file content by new lines
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-    // Remove the header line
-    /* const headers = lines.shift(); */
-
-    // Initialize an object to store student count per field
-    const fieldCounts = {};
-    const students = {};
-
-    lines.forEach((line) => {
-      const [firstname, , , field] = line.split(',');
-
-      if (!fieldCounts[field]) {
-        fieldCounts[field] = 0;
-        students[field] = [];
-      }
-
-      fieldCounts[field] += 1;
-      students[field].push(firstname);
-    });
-
-    const totalStudents = Object.values(fieldCounts).reduce((acc, curr) => acc + curr, 0);
-
-    console.log(`Number of students: ${totalStudents}`);
-    for (const field in fieldCounts) {
-      if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
-        console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${students[field].join(', ')}`);
-      }
+    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
+    }
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-}
+};
 
 module.exports = countStudents;
