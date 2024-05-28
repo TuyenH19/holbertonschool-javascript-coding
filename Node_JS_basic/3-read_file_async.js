c6af1a1c3125c8d4681e58cfa1258ccb7efd3895
TuyenH <thtuyen199@gmail.com>
2024-05-28 12:28:15 +0000

    rewrite le code

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,40 +1,33 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8')
-      .then((data) => {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const fieldCounts = {};
-        const students = {};
-
-        lines.forEach((line) => {
-          const [firstname, , , field] = line.split(',');
-
-          if (!fieldCounts[field]) {
-            fieldCounts[field] = 0;
-            students[field] = [];
-          }
-
-          fieldCounts[field] += 1;
-          students[field].push(firstname);
-        });
-
-        const totalStudents = Object.values(fieldCounts).reduce((acc, curr) => acc + curr, 0);
-
-        console.log(`Number of students: ${totalStudents}`);
-        for (const field in fieldCounts) {
-          if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
-            console.log(`Number of students in ${field}: ${fieldCounts[field]}. List: ${students[field].join(', ')}`);
-          }
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, fileData) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      try {
+        const data = fileData.split('\n').slice(1, fileData.length - 1);
+        console.log(`Number of students: ${data.length}`);
+        const subjects = {};
+        for (const row of data) {
+          const student = row.split(',');
+          if (!subjects[student[3]]) subjects[student[3]] = [];
+          subjects[student[3]].push(student[0]);
         }
-
-        resolve();
-      })
-      .catch(() => {
-        reject(new Error('Cannot load the database'));
-      });
+        const result = {
+          totalStudents: data.length,
+          subjects,
+        };
+        for (const subject in subjects) {
+          if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+        }
+        resolve(result); // Be called when the operation is successful
+      } catch (error) {
+        // Be called if an error occurs
+        reject(new Error('Error processing data'));
+      }
+    }
   });
-}
+});
 
 module.exports = countStudents;
