e8dd950f5f41a8b41f6e164df269b3e112d40803
TuyenH <thtuyen199@gmail.com>
2024-05-28 09:14:09 +0000

    Task6- Create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.use( express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello Holberton School!');
+});
+app.listen(port);
+module.exports = app;
