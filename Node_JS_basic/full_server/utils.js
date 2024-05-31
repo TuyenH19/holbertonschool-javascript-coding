ada2dcf9d7af3b157d9976fd19a3aa7be3556b16
TuyenH <thtuyen199@gmail.com>
2024-05-31 13:53:52 +0000

    update code

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,29 +1,27 @@
 // utils.js
 const fs = require('fs');
 
-const readDatabase = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, fileData) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      } else {
-        try {
-          const data = fileData.split('\n').slice(1).filter(line => line.trim() !== '');
-          const subjects = {};
-          
-          for (const row of data) {
-            const [firstname, , , field] = row.split(',');
-            if (!subjects[field]) subjects[field] = [];
-            subjects[field].push(firstname);
-          }
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, fileData) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+    } else {
+      try {
+        const data = fileData.split('\n').slice(1).filter((line) => line.trim() !== '');
+        const subjects = {};
 
-          resolve(subjects);
-        } catch (error) {
-          reject(new Error('Error processing data'));
+        for (const row of data) {
+          const [firstname, , , field] = row.split(',');
+          if (!subjects[field]) subjects[field] = [];
+          subjects[field].push(firstname);
         }
+
+        resolve(subjects);
+      } catch (error) {
+        reject(new Error('Error processing data'));
       }
-    });
+    }
   });
-};
+});
 
 module.exports = readDatabase;
